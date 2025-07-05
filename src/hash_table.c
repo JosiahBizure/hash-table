44306f88c4bec04922d34d0d5708b7709c1a40ae
Josiah Bizure <josiahbizure@gmail.com>
2025-07-04 20:42:03 -0700

    Implemented hashing and collision resolution with double hashing

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,6 +1,10 @@
 #include <string.h>
+#include <math.h>
 
 #include "hash_table.h"
+
+static const size_t HT_PRIME1 = 151;
+static const size_t HT_PRIME2 = 163;
 
 // Constructs a new key-value pair. Duplicates strings onto the heap
 static ht_item* ht_new_item(const char* k, const char* v) {
@@ -37,4 +41,22 @@ void ht_del_hash_table(ht_hash_table* ht) {
 
     free(ht->items);
     free(ht);
+}
+
+// Polynomial hash function that takes a string as input and returns the hashed index
+static size_t ht_hash(const char* str, const size_t a, const size_t num_buckets) {
+    size_t hash = 0;
+    const size_t len = strlen(str);
+    for (size_t i = 0; i < len; ++i) {
+        hash += (size_t)pow(a, len - (i+1)) * str[i];
+        hash = hash % num_buckets;
+    }
+    return hash;
+}
+
+// Function to handle collisions (open addressing with double hashing)
+static size_t ht_get_hash(const char* str, const size_t num_buckets, const size_t attempt) {
+    const size_t hash_a = ht_hash(str, HT_PRIME1, num_buckets);
+    const size_t hash_b = ht_hash(str, HT_PRIME2, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
